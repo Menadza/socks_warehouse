b498202add60d3b773d36563f642d51443aed61c
Denis <menadza000@gmail.com>
2021-10-08 09:46:03 +0300

    hard reset commit to final version

--- src/main/java/com/mn000009/warehouse/exception/IllegalColorException.java
+++ src/main/java/com/mn000009/warehouse/exception/IllegalColorException.java
@@ -3,7 +3,7 @@ package com.mn000009.warehouse.exception;
 public class IllegalColorException extends IllegalArgumentException {
 
   public IllegalColorException(String s) {
-    super("Сolor field cannot be empty or not specified. \nCurrent value of color - " + s);
+    super("Сolor field cannot be empty or not specified.\nCurrent value of color - " + s);
   }
 
 }
